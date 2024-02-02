4e0cc7064dcd2aab0cf697e62263b14faef0a2a9
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-02-02 12:03:32 -0300

    Defined constants for explicit waiting times

--- src/WebManager.cs
+++ src/WebManager.cs
@@ -3,6 +3,9 @@ using OpenQA.Selenium.Chrome;
 namespace automation;
 public class Program : IDisposable
 {
+  private const int ESPERA_LONGA = 5000;
+  private const int ESPERA_MEDIA = 3000;
+  private const int ESPERA_CURTA = 1500;
   private readonly ChromeDriver driver;
   private readonly Configuration configuration;
   public Program(Configuration configuration)
@@ -18,32 +21,33 @@ public class Program : IDisposable
       this.driver.FindElement(By.Id("username")).SendKeys(configuration.usuario);
       this.driver.FindElement(By.Id("password")).SendKeys(configuration.palavra);
       this.driver.FindElement(By.Id("sign-in")).Click();
-      System.Threading.Thread.Sleep(3000);
+      System.Threading.Thread.Sleep(ESPERA_CURTA);
     }
     if (this.driver.FindElements(By.Name("loginfmt")).Any())
     {
       this.driver.FindElements(By.Name("loginfmt")).Single().SendKeys(configuration.usuario);
       this.driver.FindElement(By.Id("idSIButton9")).Click();
-      System.Threading.Thread.Sleep(3000);
+      System.Threading.Thread.Sleep(ESPERA_CURTA);
       this.driver.FindElements(By.Name("passwd")).Single().SendKeys(configuration.palavra);
       this.driver.FindElement(By.Id("idSIButton9")).Click();
-      System.Threading.Thread.Sleep(3000);
+      System.Threading.Thread.Sleep(ESPERA_CURTA);
     }
     if (this.driver.FindElements(By.Id("lightbox")).Any())
     {
       var xpath_account = "//*[@id='tilesHolder']/div[1]/div/div[1]/div/div[1]/img";
       this.driver.FindElement(By.XPath(xpath_account)).Click();
-      System.Threading.Thread.Sleep(3000);
+      System.Threading.Thread.Sleep(ESPERA_CURTA);
       this.driver.FindElements(By.Name("passwd")).Single().SendKeys(configuration.palavra);
       this.driver.FindElement(By.Id("idSIButton9")).Click();
-      System.Threading.Thread.Sleep(3000);
+      System.Threading.Thread.Sleep(ESPERA_CURTA);
     }
     if (this.driver.FindElements(By.Name("DontShowAgain")).Any())
     {
       this.driver.FindElements(By.Name("DontShowAgain")).Single().Click();
       this.driver.FindElement(By.Id("idSIButton9")).Click();
-      System.Threading.Thread.Sleep(5000);
+      System.Threading.Thread.Sleep(ESPERA_CURTA);
     }
+    System.Threading.Thread.Sleep(ESPERA_LONGA);
   }
   public void Inicializar()
   {
