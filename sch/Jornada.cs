88fbb7f882882006a50c624499e686043a890e3b
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-02-14 10:00:45 -0300

    Added classes to receive collected information

---
+++ sch/Jornada.cs
@@ -0,0 +1,14 @@
+namespace automation.schemas;
+public class Jornada
+{
+
+}
+
+
+/*
+  <div
+    class="toaGantt-queuetoaGantt-queue -end"
+    style="left: -98865px"
+    start="-99999"
+  ></div>
+*/
