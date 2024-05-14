c63575d4bd8d53fb9f115554455f600254270d88
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-05-14 10:54:54 -0300

    Added method to collect element paths into a dictionary and added necessary properties

--- src/Configuration.cs
+++ src/Configuration.cs
@@ -1,19 +1,20 @@
-using System.Reflection;
 using OpenQA.Selenium.Chrome;
-
-namespace automation;
+namespace Automation;
 public class Configuration
 {
   public const int ESPERA_LONGA = 5000;
   public const int ESPERA_MEDIA = 3000;
   public const int ESPERA_CURTA = 1500;
+  public const int ESPERA_TOTAL = 60_000;
   public readonly string usuario;
   public readonly string palavra;
-  public readonly ChromeOptions options;
+  public readonly ChromeOptions options = new();
   public readonly string caminho;
   public readonly string website;
   public readonly string gchrome;
   public readonly string recurso;
+  public readonly Dictionary<String, String> pathfind = new();
+  public Int32 minutes_per_pixel { get; set; }
   public Configuration()
   {
     var configuracoes = ArquivoConfiguracao();
@@ -23,11 +24,11 @@ public class Configuration
     this.recurso = configuracoes["RECURSO"];
     this.gchrome = configuracoes["GCHROME"];
     this.caminho = @$"{System.IO.Directory.GetCurrentDirectory()}\www";
-    this.options = new ChromeOptions();
     this.options.AddArgument($@"--user-data-dir={this.caminho}");
     this.options.AddArgument($@"--app={this.website}");
     this.options.BinaryLocation = this.gchrome;
     VerificarAtributos();
+    DefinicaoCaminho();
   }
   private void VerificarAtributos()
   {
@@ -52,11 +53,24 @@ public class Configuration
       if (line == null) continue;
       if (line == String.Empty) continue;
       var args = line.Split('=');
-      if (args.Length < 2) throw new IndexOutOfRangeException("O arquivo contém valores inválidos!");
+      if (args.Length != 2) throw new IndexOutOfRangeException("O arquivo contém valores inválidos!");
       var cfg = args[0];
       var val = args[1];
       parametros.Add(cfg, val);
     }
     return parametros;
+  }
+  private void DefinicaoCaminho()
+  {
+    var file = System.IO.File.ReadAllLines("path.conf");
+    foreach (var line in file)
+    {
+      if(String.IsNullOrEmpty(line)) continue;
+      var args = line.Split('=');
+      if (args.Length != 2) continue;
+      var cfg = args[0];
+      var val = args[1];
+      this.pathfind.Add(cfg, val);
+    }
   }
 }
