5f709fe276b90c2e1b294e54a7b65e736fb8c202
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-05-15 08:17:45 -0300

    Fixed variable type to contain the resource name

--- src/Persistence/Espelho.cs
+++ src/Persistence/Espelho.cs
@@ -3,7 +3,7 @@ namespace Automation.Persistence
 {
   public class Espelho
   {
-    public Int32 recurso { get; set; }
+    public String recurso { get; set; }
     public Int32 par_pid { get; set; }
     public Int32 shift_start { get; set; }
     public Int32 shift_dur { get; set; }
@@ -12,7 +12,6 @@ namespace Automation.Persistence
     public Int32 final_dur { get; set; }
     public Boolean tw_alert_display { get; set; }
     public Int32 tw_alert_left { get; set; }
-    public Boolean tw_estimation_alert { get; set; }
     public Int32 queue_start_left { get; set; }
     public Int32 queue_start_start { get; set; }
     public Int32 queue_reactivated_left { get; set; }
@@ -23,7 +22,7 @@ namespace Automation.Persistence
     public List<Roteiro> roteiro { get; set; } = new();
     public Espelho(String recurso, Int32 par_pid)
     {
-      this.recurso = Int32.Parse(Regex.Match(recurso, "[0-9]{3}").Value);
+      this.recurso = recurso.Trim();
       this.par_pid = par_pid;
     }
   }
