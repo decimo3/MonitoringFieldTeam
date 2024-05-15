7c0e87d94a88e3687106c9721b54aabfb798f73f
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-05-15 08:20:39 -0300

    Added methods for comparing and exchanging variables for the next analysis

---
+++ src/WebHandler/Comparar.cs
@@ -0,0 +1,16 @@
+namespace Automation.WebScraper
+{
+  public partial class Manager
+  {
+    private void Comparar()
+    {
+      return;
+      throw new NotImplementedException();
+    }
+    private void Renovar()
+    {
+      this.velho = this.atual;
+      this.atual = new();
+    }
+  }
+}
