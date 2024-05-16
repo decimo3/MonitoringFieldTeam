d50ac6f38fd64d2af908b4cf090f246a3a6d3b8a
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-05-16 08:31:28 -0300

    Unified `initialize` and `update` methods

--- src/WebHandler/Atualizar.cs
+++ src/WebHandler/Atualizar.cs
@@ -6,7 +6,20 @@ public partial class Manager
   {
     // Selecionar o balde correto conforme parâmetro RECURSO
     this.driver.FindElements(By.ClassName("rtl-prov-name")).Where(e => e.Text == this.configuration.recurso).First().Click();
+    System.Threading.Thread.Sleep(configuration.ESPERA_MEDIA);
+    // Selecionar a visualização do gráfico de Gantt
+    this.driver.FindElements(By.ClassName("oj-ux-ico-clock")).First().Click();
+    // Abrir menu de seleção de preferências
+    this.driver.FindElements(By.ClassName("toolbar-item")).Where(e => e.Text == "Exibir").First().Click();
+    // Selecionar para exibir de forma herarquica
+    var checkbox_hierarquico = this.driver.FindElement(By.XPath(this.configuration.pathfind["CHECK_TREE"]));
+    if(!checkbox_hierarquico.Selected) checkbox_hierarquico.Click();
+    // Selecionar para exibir a rota do recurso
+    var checkbox_exibir_rota = this.driver.FindElement(By.XPath(this.configuration.pathfind["CHECK_ROUTE"]));
+    if(!checkbox_exibir_rota.Selected) checkbox_exibir_rota.Click();
+    // Aplicar as preferências de seleções
+    this.driver.FindElements(By.ClassName("app-button-title")).Where(e => e.Text == "Aplicar").First().Click();
     System.Threading.Thread.Sleep(configuration.ESPERA_LONGA);
-    // TODO - Calcular a quantidade de minutos em um pixel de deslocamento
+    this.agora = DateTime.Now;
   }
 }
