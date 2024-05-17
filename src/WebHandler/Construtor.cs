23494bd648f2ee7c75dccd5df480ff656e6e1edc
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-05-17 15:57:32 -0300

    Getting around the problem with the Chromedriver path from the development environment to the production environment

--- src/WebHandler/Construtor.cs
+++ src/WebHandler/Construtor.cs
@@ -4,17 +4,22 @@ namespace Automation.WebScraper;
 public partial class Manager : IDisposable
 {
   private readonly ChromeDriver driver;
+  private readonly ChromeDriverService service;
   private readonly Configuration configuration;
   public DateTime agora { get; set; } = DateTime.Now;
   public List<Espelho> espelhos { get; set; } = new();
   public Dictionary<String, String> relatorios { get; set; } = new();
   public Int32 horario_atual { get; set; }
-  public Int32 pixels_por_hora { get; set; }
+  public Double pixels_por_hora { get; set; }
+  public Double pixels_por_minuto { get; set; }
   public Manager(Configuration configuration)
   {
-    this.configuration = configuration;
-    this.driver = new ChromeDriver(this.configuration.options);
+    this.service = configuration.is_development ?
+      ChromeDriverService.CreateDefaultService() :
+      ChromeDriverService.CreateDefaultService(System.IO.Directory.GetCurrentDirectory());
+    this.driver = new ChromeDriver(this.service, configuration.options);
     this.driver.Manage().Window.Maximize();
+    this.configuration = configuration;
   }
   protected virtual void Dispose(bool disposing)
   {
