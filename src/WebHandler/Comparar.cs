2edb92a50f7538fc9c68ba0f4c67644c2d80c69d
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-05-24 11:27:50 -0300

    Added check if window time is greater than minimum time

--- src/WebHandler/Comparar.cs
+++ src/WebHandler/Comparar.cs
@@ -11,6 +11,13 @@ namespace Automation.WebScraper
         var janela_final = espelho.shift_left + espelho.shift_width;
         var nota_atual = espelho.servicos.Where(s => s.data_activity_status != (int)Servico.Status.pending).OrderByDescending(r => r.start).FirstOrDefault();
         var rota_atual = espelho.roteiro.OrderByDescending(r => r.start).ThenByDescending(r => r.dur).FirstOrDefault();
+        // DONE - Verificar se a janela do recurso está com horário mínimo
+        var distancia_do_tamanho_da_janela = janela_final - espelho.shift_left;
+        if(distancia_do_tamanho_da_janela < (this.pixels_por_hora * 9))
+        {
+          this.relatorios.Add(espelho.recurso, $"janela encurtada! ~{(int)(distancia_do_tamanho_da_janela/this.pixels_por_minuto)}min");
+          continue;
+        }
         // DONE - Verificar se o recurso já está na janela de horário
         if(espelho.shift_left > this.horario_atual) continue;
         // DONE - Verificar se o recurso já finalizou a rota e se ainda está na janela de horário
