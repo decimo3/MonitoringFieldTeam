c989bcc4d36c46656f5ee3619623fe8a4450b258
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-05-27 13:17:06 -0300

    Changed bucket search method to allow opening child buckets for inspection

--- src/WebHandler/Atualizar.cs
+++ src/WebHandler/Atualizar.cs
@@ -4,12 +4,40 @@ public partial class Manager
 {
   public void Atualizar()
   {
-    // Selecionar o balde correto conforme parâmetro RECURSO
-    var baldes = this.driver.FindElements(By.ClassName("rtl-prov-name"));
-    // Verifica se foi encontrado algum elemento 
-    if(!baldes.Any()) System.Environment.Exit(1);
-    var balde = this.configuration.recurso[this.configuration.contador_de_baldes];
-    baldes.Where(e => e.Text == balde).First().Click();
+    // Verifica se não foi direcionado a página de logout
+    if(this.driver.Url != this.configuration.website) System.Environment.Exit(1);
+    var balde_nome = this.configuration.recurso[this.configuration.contador_de_baldes];
+    var sub_baldes = balde_nome.Split('>');
+    var baldes = this.driver.FindElements(By.ClassName("edt-item"));
+    var i = 0;
+    var j = 0;
+    while(true)
+    {
+      var texto = baldes[i].GetAttribute("innerText");
+      if(String.IsNullOrEmpty(texto)) continue;
+      if(texto.Contains(sub_baldes[j]))
+      {
+        if(j > 0 && texto.Contains(sub_baldes[j - 1]))
+        {
+          i++;
+          continue;
+        }
+        if(j == (sub_baldes.Count() - 1))
+        {
+          baldes[i].Click();
+          break;
+        }
+        var tree_arrow = baldes[i].FindElements(By.XPath("./div/button")).First();
+        if(tree_arrow.GetAttribute("class").Contains("ptplus")) tree_arrow.Click();
+        System.Threading.Thread.Sleep(configuration.ESPERA_CURTA);
+        baldes = this.driver.FindElements(By.ClassName("edt-item"));
+        i = 0;
+        j++;
+        continue;
+      }
+      i++;
+    }
+
     System.Threading.Thread.Sleep(configuration.ESPERA_MEDIA);
     // Selecionar a visualização do gráfico de Gantt
     this.driver.FindElements(By.ClassName("oj-ux-ico-clock")).First().Click();
