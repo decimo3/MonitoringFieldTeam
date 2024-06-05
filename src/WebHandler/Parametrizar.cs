c2a750b15340d211239cb51b61c5f067822c9af7
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-06-05 09:25:02 -0300

    Added instruction to end the program if a final report was generated and the day was over

--- src/WebHandler/Parametrizar.cs
+++ src/WebHandler/Parametrizar.cs
@@ -31,7 +31,9 @@ namespace Automation.WebScraper
       this.balde_nome = this.cfg.PISCINAS[this.contador_de_baldes].Split('>').Last();
       var filename_done = $"{this.cfg.DOWNFOLDER}\\{this.datalabel.ToString("yyyyMMdd")}_{this.balde_nome}.done.csv";
       var filename_send = $"{this.cfg.DOWNFOLDER}\\{this.datalabel.ToString("yyyyMMdd")}_{this.balde_nome}.send.csv";
-      return System.IO.File.Exists(filename_done) || System.IO.File.Exists(filename_send);
+      var tem_finalizacao = System.IO.File.Exists(filename_done) || System.IO.File.Exists(filename_send);
+      if(tem_finalizacao == true && this.datalabel < DateOnly.FromDateTime(DateTime.Now)) System.Environment.Exit(0);
+      return tem_finalizacao;
     }
   }
 }
