9a6b9fc8fc71e92f2637da3300b4ae1bbf5e0d54
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-08-07 13:00:55 -0300

    Added class to check for improper parallel execution and terminate residual processes (if any)

---
+++ src/Helpers/Verificador.cs
@@ -0,0 +1,28 @@
+namespace Automation.Helpers;
+public static class Verificador
+{
+  public static void Verificar()
+  {
+    System.Threading.Thread.Sleep(5_000);
+    Console.WriteLine($"{DateTime.Now} - Verificando se há outras instâncias do sistema...");
+    var result = Helpers.Executor.Executar("tasklist", $"/NH /FI \"IMAGENAME eq ofs.exe\"");
+    if(((result.Length - result.Replace("ofs.exe", "").Length) / "ofs.exe".Length) > 1)
+    {
+      Console.WriteLine($"{DateTime.Now} - Já existe uma instância do sistema em execução!");
+      System.Environment.Exit(409);
+    }
+    Console.WriteLine($"{DateTime.Now} - Finalizando processos residuais (se houver)...");
+    var exec2kill = new List<String> {"chrome.exe", "chromedriver.exe"};
+    foreach (var exec in exec2kill)
+    {
+      try
+      {
+        Helpers.Executor.Executar("taskkill", $"/F /T /IM {exec}");
+      }
+      catch (System.Exception erro)
+      {
+        Console.WriteLine(erro.Message);
+      }
+    }
+  }
+}
