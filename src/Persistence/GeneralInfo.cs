f100da4c1b1a0afbabdd9c6769fef339db89d820
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-09-19 08:21:24 -0300

    refactor(Persistence): add some properties to map information

--- src/Persistence/GeneralInfo.cs
+++ src/Persistence/GeneralInfo.cs
@@ -8,8 +8,9 @@ namespace Automation.Persistence
     public string NotaServico { get; set; }
     public string Recurso { get; set; }
     public string Atividade { get; set; }
-    public string Servico { get; set; }
-    public string Estado { get; set; }
+    public string Situacao { get; set; }
+    public string Damage { get; set; }
+    public string Vencimento { get; set; }
     public string Descricao { get; set; }
     public string Observacao { get; set; }
     public override string ToString()
@@ -18,9 +19,11 @@ namespace Automation.Persistence
       if(Data is not null) builder.Append($"Data: {Data}\n");
       if(Recurso is not null) builder.Append($"Recurso: {Recurso}\n");
       if(Atividade is not null) builder.Append($"Atividade: {Atividade}\n");
-      if(Servico is not null) builder.Append($"Serviço: {Servico}\n");
+      if(Vencimento is not null) builder.Append($"Vencimento: {Vencimento}\n");
+      if(NotaServico is not null) builder.Append($"NotaServico: {NotaServico}\n");
+      if(Damage is not null) builder.Append($"Damage: {Damage}\n");
       if(Descricao is not null) builder.Append($"Descrição: {Descricao}\n");
-      if(Estado is not null) builder.Append($"Estado: {Estado}\n");
+      if(Situacao is not null) builder.Append($"Situação: {Situacao}\n");
       if(Observacao is not null) builder.Append($"Observação: {Observacao}\n");
       return builder.ToString();
     }
