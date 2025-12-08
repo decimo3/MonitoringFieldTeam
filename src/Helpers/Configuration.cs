e130d35385cf5248c5d286114d1bf6ee932478e2
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-12-08 15:49:15 -0300

    feat: add ValueNotFoundException class to Configuration.cs

--- src/Helpers/Configuration.cs
+++ src/Helpers/Configuration.cs
@@ -1,5 +1,14 @@
 namespace MonitoringFieldTeam.Helpers;
 
+public class ValueNotFoundException : Exception
+{
+  public ValueNotFoundException() { }
+  public ValueNotFoundException(string message) : base(message) { }
+  public ValueNotFoundException(string message, Exception innerException) : base(message, innerException) { }
+  protected ValueNotFoundException(
+    System.Runtime.Serialization.SerializationInfo info,
+    System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
 public class Configuration
 {
   public readonly String DATAFOLDER;
