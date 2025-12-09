b5bacd6c2888256fd361327a68cb6b69a2d1c340
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-12-09 10:16:04 -0300

    refacto: update Configuration implementation on Autenticador class

--- src/WebHandler/Autenticar.cs
+++ src/WebHandler/Autenticar.cs
@@ -5,21 +5,23 @@ namespace MonitoringFieldTeam.WebScraper;
 
 public static class Autenticador
 {
-  public static void Autenticar(WebHandler.WebHandler handler, Configuration cfg)
+  public static void Autenticar(WebHandler.WebHandler handler)
   {
+    var usuario = Configuration.GetString("USUARIO");
+    var palavra = Configuration.GetString("PALAVRA");
     if (handler.GetElements("AUTENTICAR_OFSAUTH", WAITSEC.Medio).Any())
     {
-      handler.GetElement("AUTENTICAR_OFSUSER", WAITSEC.Agora).SendKeys(cfg.CONFIGURACAO["USUARIO"]);
-      handler.GetElement("AUTENTICAR_OFSPASS", WAITSEC.Agora).SendKeys(cfg.CONFIGURACAO["PALAVRA"]);
+      handler.GetElement("AUTENTICAR_OFSUSER", WAITSEC.Agora).SendKeys(usuario);
+      handler.GetElement("AUTENTICAR_OFSPASS", WAITSEC.Agora).SendKeys(palavra);
       handler.GetElement("AUTENTICAR_OFSLOGIN", WAITSEC.Agora).Click();
     }
     if (handler.GetElements("AUTENTICAR_WFMAUTH", WAITSEC.Medio).Any())
     {
-      handler.GetElement("AUTENTICAR_WFMUSER", WAITSEC.Curto).SendKeys(cfg.CONFIGURACAO["USUARIO"]);
+      handler.GetElement("AUTENTICAR_WFMUSER", WAITSEC.Curto).SendKeys(usuario);
       handler.GetElement("AUTENTICAR_WFMBUTTON", WAITSEC.Agora).Click();
       if (handler.GetElements("AUTENTICAR_WFMERROR1", WAITSEC.Curto).Any())
         throw new InvalidOperationException("O usuário informado está incorreto!");
-      handler.GetElement("AUTENTICAR_WFMPASS", WAITSEC.Curto).SendKeys(cfg.CONFIGURACAO["PALAVRA"]);
+      handler.GetElement("AUTENTICAR_WFMPASS", WAITSEC.Curto).SendKeys(palavra);
       handler.GetElement("AUTENTICAR_WFMBUTTON", WAITSEC.Agora).Click();
       if (handler.GetElements("AUTENTICAR_WFMERROR2", WAITSEC.Curto).Any())
         throw new InvalidOperationException("A senha informada está incorreta!");
@@ -27,7 +29,7 @@ public static class Autenticador
     if (handler.GetElements("AUTENTICAR_WFMLIST", WAITSEC.Medio).Any())
     {
       handler.GetElement("AUTENTICAR_WFMLIST", WAITSEC.Agora).Click();
-      handler.GetElement("AUTENTICAR_WFMPASS", WAITSEC.Curto).SendKeys(cfg.CONFIGURACAO["PALAVRA"]);
+      handler.GetElement("AUTENTICAR_WFMPASS", WAITSEC.Curto).SendKeys(palavra);
       handler.GetElement("AUTENTICAR_WFMBUTTON", WAITSEC.Agora).Click();
       if (handler.GetElements("AUTENTICAR_WFMERROR2", WAITSEC.Curto).Any())
         throw new InvalidOperationException("A senha informada está incorreta!");
