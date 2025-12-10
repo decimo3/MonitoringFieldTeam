f07ed187a92d68871661a183ac6f00efe8c420a6
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-12-10 14:13:26 -0300

    refactor: simplify method for generating CSV files.

--- src/Helpers/TableMaker.cs
+++ src/Helpers/TableMaker.cs
@@ -1,55 +1,33 @@
 using System.Collections;
 namespace MonitoringFieldTeam.Helpers;
 
-public static class TableMaker<T>
+public static class TableMaker
 {
-  public static readonly char separador = ';';
-  public static String Serialize(List<T> relatorios)
+  public const char SEPARADOR = ';';
+  public static string ListObjectsToCSV(IList list)
   {
-    if (relatorios == null || relatorios.Count == 0)
-      throw new ArgumentException("The list of reports cannot be null or empty.");
+    if (list == null || list.Count == 0)
+      throw new ArgumentException($"A lista está vazia!");
+    var type = list[0]!.GetType();
+    var properties = type.GetProperties();
     var table = new System.Text.StringBuilder();
-    var cabecalhos = Cabecalho(relatorios.First());
-    table.Append(String.Join(separador, cabecalhos.Keys.ToList()));
-    table.Append('\n');
-    foreach(var relatorio in relatorios)
+    var values = new List<string>();
+    foreach (var property in properties)
     {
-      table.Append(String.Join(separador, Rodape(relatorio, cabecalhos)));
-      table.Append('\n');
+      values.Add(property.Name);
+    }
+    table.AppendLine(string.Join(SEPARADOR, values));
+    values.Clear();
+    foreach (var item in list)
+    {
+      foreach (var property in properties)
+      {
+        var value = property.GetValue(item);
+        values.Add(value?.ToString() ?? string.Empty);
+      }
+      table.AppendLine(string.Join(SEPARADOR, values));
+      values.Clear();
     }
     return table.ToString();
-  }
-  public static Dictionary<String, Int32> Cabecalho(T obj)
-  {
-    if (obj == null)
-      throw new ArgumentException("The report cannot be null.");
-    var contador = 0;
-    Type tipo = obj.GetType();
-    var cabecalhos = new Dictionary<String, Int32>();
-    var atributos = tipo.GetProperties();
-    foreach(var atributo in atributos)
-    {
-      cabecalhos.Add(atributo.Name, contador);
-      contador++;
-    }
-    return cabecalhos;
-  }
-  public static List<String> Rodape(T obj, Dictionary<String,Int32> keys)
-  {
-    if (obj == null)
-      throw new ArgumentException("The report cannot be null.");
-    Type tipo = obj.GetType();
-    var rodape = new List<String>(new string[keys.Count]);
-    var atributos = tipo.GetProperties();
-    foreach(var atributo in atributos)
-    {
-      var index = keys[atributo.Name];
-      var value = atributo.GetValue(obj);
-      if(value != null)
-      {
-        rodape[index] = value.ToString() ?? String.Empty;
-      }
-    }
-    return rodape;
   }
 }
