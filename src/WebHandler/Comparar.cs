7b2ee07cdf2502dd2048435367fe59eff4614647
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-12-10 10:30:49 -0300

    refactor: changed instanced properties to argument value

--- src/WebHandler/Comparar.cs
+++ src/WebHandler/Comparar.cs
@@ -4,12 +4,15 @@ using MonitoringFieldTeam.Persistence;
 
 namespace MonitoringFieldTeam.WebScraper
 {
-  public partial class Manager
+  public static class Comparador
   {
-    public void Comparar()
+    private const int TOLERANCIA = 3;
+    public static List<FeedBack> Comparar(List<Espelho> espelhos, int horario_atual, int pixels_por_minuto)
     {
-      foreach (var espelho in this.espelhos)
+      var relatorios = new List<FeedBack>();
+      foreach (var espelho in espelhos)
       {
+        if (string.IsNullOrEmpty(espelho.recurso)) continue;
         var janela_final = espelho.shift_left + espelho.shift_width;
         var janela_tamanho = janela_final - espelho.shift_left;
         var notas_pendentes = espelho.servicos.Where(s =>
@@ -24,159 +27,153 @@ namespace MonitoringFieldTeam.WebScraper
         var rota_atual = espelho.roteiros.OrderByDescending(r => r.start).ThenByDescending(r => r.dur).FirstOrDefault();
         var nota_atual = notas_pendentes.FirstOrDefault();
         // DONE - Verificar se o recurso já está na janela de horário
-        if(this.horario_atual < espelho.shift_left && espelho.queue_start_start < 0) continue;
-        if(this.horario_atual > janela_final && espelho.queue_end_start > 0) continue;
+        if (horario_atual < espelho.shift_left && espelho.queue_start_start < 0) continue;
+        if (horario_atual > janela_final && espelho.queue_end_start > 0) continue;
         // DONE - Verificar se a janela do recurso está com horário mínimo
-        Int32 minutos_esperados_para_a_janela = 0;
-        var prefixo_do_recurso = espelho.recurso.Substring(0, espelho.recurso.Length - 3);
-        var numero_do_recurso = Int32.Parse(espelho.recurso.Substring(espelho.recurso.Length - 3));
-        minutos_esperados_para_a_janela = EncontrarChave(this.cfg.HORARIOS, prefixo_do_recurso, numero_do_recurso);
-        if(minutos_esperados_para_a_janela == 0) continue;
-        if(espelho.queue_start_start > 0 && espelho.queue_end_start < 0)
+        var prefixo_do_recurso = espelho.recurso[..^3];
+        var numero_do_recurso = Int32.Parse(espelho.recurso[^3..]);
+        Int32 minutos_esperados_para_a_janela = EncontrarChave(prefixo_do_recurso, numero_do_recurso);
+        if (minutos_esperados_para_a_janela == 0) continue;
+        if (espelho.queue_start_start > 0 && espelho.queue_end_start < 0)
         {
-        if(espelho.shift_dur < minutos_esperados_para_a_janela)
-        {
-          Concatenar(espelho.recurso, "jornada encurtada", espelho.shift_dur);
-        }
-        if(espelho.shift_dur > minutos_esperados_para_a_janela)
-        {
-          Concatenar(espelho.recurso, "jornada extendida", espelho.shift_dur);
-        }
+          if (espelho.shift_dur < minutos_esperados_para_a_janela)
+          {
+            relatorios.Add(new FeedBack(espelho.recurso, "jornada encurtada", espelho.shift_dur));
+          }
+          if (espelho.shift_dur > minutos_esperados_para_a_janela)
+          {
+            relatorios.Add(new FeedBack(espelho.recurso, "jornada extendida", espelho.shift_dur));
+          }
         }
         // DONE - Verificar se o recurso já finalizou a rota e se ainda está na janela de horário
-        if(espelho.queue_end_left > 0 && this.horario_atual < janela_final)
+        if (espelho.queue_end_left > 0 && horario_atual < janela_final)
         {
-          var diff = this.horario_atual - espelho.queue_end_left;
-          Concatenar(espelho.recurso, "deslogou antes", (int)(diff/this.pixels_por_minuto));
+          var diff = horario_atual - espelho.queue_end_left;
+          relatorios.Add(new FeedBack(espelho.recurso, "deslogou antes", (int)(diff / pixels_por_minuto)));
           continue;
         }
         // DONE - Verificar se o recurso já está logado ou reativado
-        if(espelho.queue_start_left < 0)
+        if (espelho.queue_start_left < 0)
         {
-          var diff = this.horario_atual - espelho.shift_left;
-          if((diff/this.pixels_por_minuto) > cfg.TOLERANCIA)
+          var diff = horario_atual - espelho.shift_left;
+          if ((diff / pixels_por_minuto) > TOLERANCIA)
           {
-            Concatenar(espelho.recurso, "ainda não logou", (int)(diff/this.pixels_por_minuto));
+            relatorios.Add(new FeedBack(espelho.recurso, "ainda não logou", (int)(diff / pixels_por_minuto)));
             continue;
           }
         }
-        if(espelho.queue_start_left > 0)
+        if (espelho.queue_start_left > 0)
         {
-          if(this.horario_atual < espelho.shift_left)
+          if (horario_atual < espelho.shift_left)
           {
             var diff = espelho.shift_left - espelho.queue_start_left;
-            Concatenar(espelho.recurso, "logou antes", (int)(diff/this.pixels_por_minuto));
+            relatorios.Add(new FeedBack(espelho.recurso, "logou antes", (int)(diff / pixels_por_minuto)));
           }
         }
-        if(espelho.queue_end_left > 0) continue;
+        if (espelho.queue_end_left > 0) continue;
         // DONE - Verificar se o recurso ainda tem notas pendentes
-        if(this.horario_atual < janela_final)
+        if (horario_atual < janela_final)
         {
-          if(nota_atual == null)
+          if (nota_atual == null)
           {
-            Concatenar(espelho.recurso, "equipe sem notas");
+            relatorios.Add(new FeedBack(espelho.recurso, "equipe sem notas", null));
             continue;
           }
-          if(notas_pendentes.Count() == 1)
+          if (notas_pendentes.Count() == 1)
           {
-            var diff = this.horario_atual - (nota_atual.style_left + nota_atual.style_width);
-            Concatenar(espelho.recurso, "na última nota", (int)(diff/this.pixels_por_minuto));
+            var diff = horario_atual - (nota_atual.style_left + nota_atual.style_width);
+            relatorios.Add(new FeedBack(espelho.recurso, "na última nota", (int)(diff / pixels_por_minuto)));
           }
         }
         // DONE - Verificar se o recurso tem alguma nota `enroute` ou `started`
-        if(nota_atual == null || nota_atual.data_activity_status == Servico.Status.pending)
+        if (nota_atual == null || nota_atual.data_activity_status == Servico.Status.pending)
         {
-          if(nota_anterior == null)
+          if (nota_anterior == null)
           {
-            var diff = this.horario_atual - espelho.shift_left;
-            Concatenar(espelho.recurso, "equipe ociosa", (int)(diff/this.pixels_por_minuto));
+            var diff = horario_atual - espelho.shift_left;
+            relatorios.Add(new FeedBack(espelho.recurso, "equipe ociosa", (int)(diff / pixels_por_minuto)));
             continue;
           }
           else
           {
-            var diff = this.horario_atual - (nota_anterior.style_left + nota_anterior.style_width);
-            if(diff/this.pixels_por_minuto >= cfg.TOLERANCIA)
+            var diff = horario_atual - (nota_anterior.style_left + nota_anterior.style_width);
+            if (diff / pixels_por_minuto >= TOLERANCIA)
             {
-              Concatenar(espelho.recurso, "equipe ociosa", (int)(diff/this.pixels_por_minuto));
+              relatorios.Add(new FeedBack(espelho.recurso, "equipe ociosa", (int)(diff / pixels_por_minuto)));
               continue;
             }
           }
         }
 
-        if(nota_atual != null && nota_atual.data_activity_status == Servico.Status.started)
+        if (nota_atual != null && nota_atual.data_activity_status == Servico.Status.started)
         {
-          if(this.horario_atual > nota_atual.style_left + nota_atual.style_width)
+          if (horario_atual > nota_atual.style_left + nota_atual.style_width)
           {
-            var diff = this.horario_atual - (nota_atual.style_left + nota_atual.style_width);
-            Concatenar(espelho.recurso, "atendimento atrasado", (int)(diff/this.pixels_por_minuto));
+            var diff = horario_atual - (nota_atual.style_left + nota_atual.style_width);
+            relatorios.Add(new FeedBack(espelho.recurso, "atendimento atrasado", (int)(diff / pixels_por_minuto)));
           }
         }
 
-        if(rota_atual == null)
+        if (rota_atual == null)
         {
-          var diff = this.horario_atual - espelho.shift_left;
-          Concatenar(espelho.recurso, "GPS desligado", (int)(diff/this.pixels_por_minuto));
+          var diff = horario_atual - espelho.shift_left;
+          relatorios.Add(new FeedBack(espelho.recurso, "GPS desligado", (int)(diff / pixels_por_minuto)));
           continue;
         }
-        var distancia_do_inicio_registro_de_rota = this.horario_atual - rota_atual.style_left;
-        var distancia_do_final_registro_de_rota = this.horario_atual - (rota_atual.style_left + rota_atual.style_width);
-        var minutos_do_inicio_registro_de_rota = Convert.ToInt32(distancia_do_inicio_registro_de_rota / this.pixels_por_minuto);
-        var minutos_do_final_registro_de_rota = Convert.ToInt32(distancia_do_final_registro_de_rota / this.pixels_por_minuto);
-        if(minutos_do_final_registro_de_rota >= cfg.TOLERANCIA)
+        var distancia_do_inicio_registro_de_rota = horario_atual - rota_atual.style_left;
+        var distancia_do_final_registro_de_rota = horario_atual - (rota_atual.style_left + rota_atual.style_width);
+        var minutos_do_inicio_registro_de_rota = Convert.ToInt32(distancia_do_inicio_registro_de_rota / pixels_por_minuto);
+        var minutos_do_final_registro_de_rota = Convert.ToInt32(distancia_do_final_registro_de_rota / pixels_por_minuto);
+        if (minutos_do_final_registro_de_rota >= TOLERANCIA)
         {
-          Concatenar(espelho.recurso, "GPS sem registro", minutos_do_final_registro_de_rota);
+          relatorios.Add(new FeedBack(espelho.recurso, "GPS sem registro", minutos_do_final_registro_de_rota));
           continue;
         }
-        if(rota_atual.status == Roteiro.Status.idle)
+        if (rota_atual.status == Roteiro.Status.idle)
         {
-          Concatenar(espelho.recurso, "parada indevida", minutos_do_inicio_registro_de_rota);
+          relatorios.Add(new FeedBack(espelho.recurso, "parada indevida", minutos_do_inicio_registro_de_rota));
           continue;
         }
-        if(rota_atual.status == Roteiro.Status.alert)
+        if (rota_atual.status == Roteiro.Status.alert)
         {
-          if(nota_atual == null)
+          if (nota_atual == null)
           {
-            Concatenar(espelho.recurso, "encerrou deslocando", minutos_do_inicio_registro_de_rota);
+            relatorios.Add(new FeedBack(espelho.recurso, "encerrou deslocando", minutos_do_inicio_registro_de_rota));
             continue;
           }
-          if(nota_atual.data_activity_status == Servico.Status.started)
+          if (nota_atual.data_activity_status == Servico.Status.started)
           {
-            Concatenar(espelho.recurso, "deslocamento indevido", minutos_do_inicio_registro_de_rota);
+            relatorios.Add(new FeedBack(espelho.recurso, "deslocamento indevido", minutos_do_inicio_registro_de_rota));
             continue;
           }
-          if(nota_atual.data_activity_status == Servico.Status.enroute)
+          if (nota_atual.data_activity_status == Servico.Status.enroute)
           {
-            Concatenar(espelho.recurso, "deslocamento atrasado", minutos_do_inicio_registro_de_rota);
+            relatorios.Add(new FeedBack(espelho.recurso, "deslocamento atrasado", minutos_do_inicio_registro_de_rota));
             continue;
           }
         }
       }
+      return relatorios;
     }
-    public void Concatenar(String recurso, String aviso, Int32? tempo = null)
+    public static Int32 EncontrarChave(String prefixo, Int32 numero)
     {
-      this.relatorios.Append(recurso);
-      this.relatorios.Append($" {aviso}\\!");
-      if(tempo != null)
-        this.relatorios.Append($" \\~{tempo}min");
-      this.relatorios.Append('\n');
-    }
-    public Int32 EncontrarChave(Dictionary<String, Int32> dicionario, String prefixo, Int32 numero)
-    {
-      if(this.cfg.HORARIOS.TryGetValue(prefixo, out Int32 minutos_esperados_para_a_janela))
-        return minutos_esperados_para_a_janela;
-      var chaves = dicionario.Keys.ToList();
+      var pairs = Configuration.GetPairs("HORARIO");
+      var pair = pairs.FirstOrDefault(c => c.Key == prefixo);
+      if (pair.Key is not null)
+        return (int)pair.Value;
+      var chaves = pairs.Select(p => p.Key).ToList();
       var padrao = @"^([A-Z]{3,5})[\[]([0-9]{1,2})\.([0-9]{1,2})[\]]$";
       var regex = new Regex(padrao);
-      foreach(var chave in chaves)
+      foreach (var kv in pairs)
       {
-        var match = regex.Match(chave);
-        if(match != null && match.Groups[1].Value == prefixo)
+        var match = regex.Match(kv.Key);
+        if (match != null && match.Groups[1].Value == prefixo)
         {
-          if(numero >= Int32.Parse(match.Groups[2].Value))
+          if (numero >= Int32.Parse(match.Groups[2].Value))
           {
-            if(numero <= Int32.Parse(match.Groups[3].Value))
+            if (numero <= Int32.Parse(match.Groups[3].Value))
             {
-              return dicionario[chave];
+              return (int)kv.Value;
             }
           }
         }
