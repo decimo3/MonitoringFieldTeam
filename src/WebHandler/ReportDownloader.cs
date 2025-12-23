6430771e1f00e8f1d16eec970b4a51b008b05e84
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-12-23 09:08:54 -0300

    refactor(dry): implement `WebHandler.DownloadFile` on `ReportDownloader.Download`

--- src/WebHandler/ReportDownloader.cs
+++ src/WebHandler/ReportDownloader.cs
@@ -15,9 +15,6 @@ public static class ReportDownloader
   public static string? Download(WebHandler.WebHandler handler, String bucketName, DateOnly date)
   {
     Parametrizador.VerificarPagina(handler);
-    var expectedFilename = $"Atividades-{bucketName}_{date.ToString("dd_MM_yy")}.csv";
-    var expectedFilepath = System.IO.Path.Combine(
-      Configuration.GetString("DATAPATH"), expectedFilename);
     if (!handler.GetElements("GANNT_ACTIONBTN").Any())
     {
       Log.Error("Não é possível baixar relatório de um grupo!");
@@ -26,12 +23,7 @@ public static class ReportDownloader
     var parent = handler.GetElement("GANNT_TOOLBAR");
     handler.GetNestedElements(parent, "GANNT_ACTIONBTN").First().Click();
     parent = handler.GetElement("GANNT_OPTIONSVIEW", WebHandler.WAITSEC.Curto);
-    handler.GetNestedElements(parent, "GANNT_EXPORTBTN").First().Click();
-    for (int i = 0; i < 3; i++)
-    {
-      if (System.IO.File.Exists(expectedFilepath)) return expectedFilepath;
-      System.Threading.Thread.Sleep(TimeSpan.FromSeconds((int)WAITSEC.Curto));
-    }
-    throw new InvalidOperationException("Nenhum relatório foi gerado!");
+    var download = handler.GetNestedElements(parent, "GANNT_EXPORTBTN").First();
+    return handler.DownloadFile(download);
   }
 }
