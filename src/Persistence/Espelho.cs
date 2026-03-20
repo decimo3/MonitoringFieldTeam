ad4658f1c7c5336c1f4358a494dee5b0ca0d2239
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-03-20 10:50:03 -0300

    refactor: remove resource name shortening and keep normalization logic

--- src/Persistence/Espelho.cs
+++ src/Persistence/Espelho.cs
@@ -31,19 +31,6 @@ namespace MonitoringFieldTeam.Persistence
       abreviado = abreviado.Replace('\u00A0', '\u0020');
       // Replace double spaces by singles
       abreviado = abreviado.Replace("  ", " ");
-      // Abbreviates words that determine the type of activity
-      abreviado = abreviado.Replace(" - Corte", "C");
-      abreviado = abreviado.Replace(" - Religa", "R");
-      abreviado = abreviado.Replace(" - Vistoriador ", "V");
-      abreviado = abreviado.Replace(" - Indica", "");
-      // Remove the word 'team' to shorten the resource name
-      abreviado = abreviado.Replace(" - Equipe ", "");
-      // remove any excess word before space character
-      var indice = abreviado.IndexOf(' ');
-      if(indice > 0)
-      {
-        abreviado = abreviado[..indice];
-      }
       this.recurso = abreviado;
       this.par_pid = par_pid;
       this.style_top = style_top;
