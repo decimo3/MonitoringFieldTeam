42de7769ef9d3bd0f673e1a26856730f92672ef2
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-04-09 16:08:04 -0300

    feat: add Delegator service mode
    Distribute orders to online workers.

---
+++ src/Helpers/Delegator.cs
@@ -0,0 +1,14 @@
+namespace MonitoringFieldTeam.Helpers;
+
+public static class Delegator
+{
+  public static void Run()
+  {
+    // TODO - Get the list of orders
+    // TODO - Get the list of workers
+    // TODO - Check witch workers are on
+    // TODO - Send orders to online workers
+    // TODO - Store successful response on DB
+    // TODO - Export the report in the end
+  }
+}
