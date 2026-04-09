1ee539b56d02aca3a325ce1444c7dfb596a400b2
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-04-09 17:14:52 -0300

    feat: get the orders list from file on DATAPATH configuration.

--- src/Helpers/Delegator.cs
+++ src/Helpers/Delegator.cs
@@ -4,7 +4,17 @@ public static class Delegator
 {
   public static void Run()
   {
-    // TODO - Get the list of orders
+    // DONE - Get the list of orders
+    Log.Information("Verificando a lista de notas...");
+    var filepath = System.IO.Path.Combine(
+      Configuration.GetString("DATAPATH"),
+      "ofs.txt");
+    if (!System.IO.File.Exists(filepath))
+    {
+      Log.Information("O arquivo de lista notas não foi encontrado!");
+      return;
+    }
+    var orders = System.IO.File.ReadAllLines(filepath);
     // TODO - Get the list of workers
     // TODO - Check witch workers are on
     // TODO - Send orders to online workers
