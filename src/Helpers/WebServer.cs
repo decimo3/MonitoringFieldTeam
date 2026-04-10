c1e9998843eb73344e96161b270a4bdc4a8e7180
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-04-10 11:06:20 -0300

    feat: add support to serve static files on DATAPATH folder.

--- src/Helpers/WebServer.cs
+++ src/Helpers/WebServer.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using MonitoringFieldTeam.Persistence;
 using MonitoringFieldTeam.WebScraper;
@@ -11,11 +12,17 @@ namespace MonitoringFieldTeam.Helpers;
 public static class WebServer
 {
   private static object _lock = new();
+  private static readonly string ROOT = Configuration.GetString("DATAPATH");
   public static void Run(WebHandler.WebHandler handler)
   {
     var builder = WebApplication.CreateBuilder();
     builder.WebHost.UseUrls("http://localhost:7826");
     var app = builder.Build();
+    app.UseStaticFiles(new StaticFileOptions
+    {
+      RequestPath = "/",
+      FileProvider = new PhysicalFileProvider(ROOT)
+    });
     app.MapGet("/", (HttpContext context) =>
     {
       using var reader = new StreamReader(context.Request.Body);
