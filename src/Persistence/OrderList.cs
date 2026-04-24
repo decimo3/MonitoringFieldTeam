b96e90beffd0d903f5133fe1a43c7ea2efdef2b2
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-04-24 14:06:58 -0300

    feat: add OrderInfo data model to Persistence namespace

---
+++ src/Persistence/OrderList.cs
@@ -0,0 +1,11 @@
+namespace MonitoringFieldTeam.Persistence;
+
+public class OrderInfo
+{
+  public long Identifier { get; set; } = 0;
+  public long OrderNumber { get; set; } = 0;
+  public int StatusCode { get; set; } = 0;
+  public DateTime CreatedAt { get; set; } = DateTime.Now;
+  public DateTime UpdatedAt { get; set; } = DateTime.Now;
+  public string? Observation { get; set; }
+}
