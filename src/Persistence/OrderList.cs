bef78522ada192dd28600143141391c4a730c8fa
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-04-29 17:09:11 -0300

    feat: add property to hold activity id on OrderInfo class

--- src/Persistence/OrderList.cs
+++ src/Persistence/OrderList.cs
@@ -3,6 +3,7 @@ namespace MonitoringFieldTeam.Persistence;
 public class OrderInfo
 {
   public long Identifier { get; set; } = 0;
+  public long ActivityId { get; set; } = 0;
   public long OrderNumber { get; set; } = 0;
   public int StatusCode { get; set; } = 0;
   public DateTime CreatedAt { get; set; } = DateTime.Now;
