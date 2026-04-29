0f5a3abfbcabd5e356707b95999bbed6f7c86544
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-04-29 17:38:17 -0300

    feat: add aid parameter to hold activity id on RequestInfo

--- src/Persistence/RequestInfo.cs
+++ src/Persistence/RequestInfo.cs
@@ -4,9 +4,11 @@ public class RequestInfo
 {
   public string[] info { get; set; }
   public long nota { get; set; }
-  public RequestInfo(string[] info, long nota)
+  public long aid { get; set; }
+  public RequestInfo(string[] info, long nota, long aid = 0)
   {
     this.info = info;
     this.nota = nota;
+    this.aid = aid;
   }
 }
